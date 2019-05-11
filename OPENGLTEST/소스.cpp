01560a26336f284cf2df7dc5890d781d3bceb8dc
KS <hki2345@naver.com>
2019-05-11 20:45:03 +0900

    19.5.11 Init
    dd

---
+++ OPENGLTEST/소스.cpp
@@ -0,0 +1,24 @@
+#include "gl/glut.h"
+
+void display() {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glBegin
+	(GL_POLYGON);
+	glVertex2f(-0.5f, -0.5f);
+	glVertex2f(0.5f, -0.5f);
+	glVertex2f(0.5f, 0.5f);
+	glVertex2f(-0.5f, 0.5f);
+	glEnd();
+	glFinish();
+}
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutCreateWindow
+	("OpenGL");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
